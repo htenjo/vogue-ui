6dad6e2d634f3bcaa3ee9621b771cff7c71ee4ee
Hernan Tenjo <htenjo@hugeinc.com>
2016-06-30 21:27:16 -0500

    Version working with @angular2 RC.4

--- main/main.ts
+++ main/main.ts
@@ -2,10 +2,4 @@ import { bootstrap }    from '@angular/platform-browser-dynamic';
 import { AppComponent } from './components/app/app';
 import { HTTP_PROVIDERS } from '@angular/http';
 
-/*
-import { enableProdMode } from '@angular/core';
-if (environment.production){
-    enableProdMode();
-}
-*/
 bootstrap(AppComponent, [HTTP_PROVIDERS]);

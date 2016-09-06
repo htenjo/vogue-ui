b167364d010b9f5e0d9defd891986a2c24e102f7
Hernan Tenjo <htenjo@hugeinc.com>
2016-09-06 09:27:04 -0500

    Remove ignored files

--- main/model/common.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var ListWrapper = (function () {
-    function ListWrapper() {
-    }
-    return ListWrapper;
-}());
-exports.ListWrapper = ListWrapper;
-//# sourceMappingURL=common.js.map
